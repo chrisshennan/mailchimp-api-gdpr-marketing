40f073a2e5a50c6d7923aa449dc7f2c46d5486eb
Chris <chris@chrisshennan.com>
2020-02-17 13:37:11 +0000

    Fixing indentation (tabs -> spaces)

--- src/subscribe.php
+++ src/subscribe.php
@@ -14,7 +14,7 @@ $listId = '[MAILCHIMP_LIST_ID]';
 
 // Marketing Preference Mapping for embedded form -> mailchimp permissions.
 $marketingPreferencesMapping = [
-	'email' => '[MARKETING_PREFERENCE_ID_FOR_EMAIL]',
+    'email' => '[MARKETING_PREFERENCE_ID_FOR_EMAIL]',
     'direct_mail' => '[MARKETING_PREFERENCE_ID_FOR_DIRECT_MAIL]',
     'customized_online_advertising' => '[MARKETING_PREFERENCE_ID_FOR_CUSTOMIZED_ONLINE_ADVERTISING',
 ];
@@ -26,8 +26,8 @@ $email = '[SUBSCRIBER_EMAIL_ADDRESS]';
 // Marketing Permissions obtained from the embeded newsletter signup form
 // We're going to assume email and customized_online_advertising was selected in the form
 $selectedMarketingPermissions = [
-	'email',
-	'customized_online_advertising',
+    'email',
+    'customized_online_advertising',
 ];
 
 /*
@@ -61,15 +61,15 @@ $contact = $mailChimp->get("lists/$listId/members/$subscriberHash");
 // Get the appropriate GDPR options
 $enabledMarketingPermissions = [];
 foreach ($contact['marketing_permissions'] as $marketingPermission) {
-	// Get the key for the marketing permission id
-	$key = array_search($marketingPermission['marketing_permission_id'], $marketingPreferencesMapping);
+    // Get the key for the marketing permission id
+    $key = array_search($marketingPermission['marketing_permission_id'], $marketingPreferencesMapping);
 
-	// If the key isn't one in the selected marketing permissions the continue to next one
-	if (!in_array($key, $selectedMarketingPermissions)) {
-		continue;
-	}
+    // If the key isn't one in the selected marketing permissions the continue to next one
+    if (!in_array($key, $selectedMarketingPermissions)) {
+        continue;
+    }
 
-	// Set the marketing permission to enabled
+    // Set the marketing permission to enabled
     $marketingPermission['enabled'] = true;
     $enabledMarketingPermissions[] = $marketingPermission;
 }
